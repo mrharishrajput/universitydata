9516c0a27b4ffda5ed9b5bf2e68b733357b1bb86
Harish Kumar <81924036+mrharishrajput@users.noreply.github.com>
2021-11-29 22:49:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React, {useState,useEffect}  from "react";
 
 import './App.css';
 
 function App() {
-  return (
+  let [api,setapi]= useState();
+  const[isloaded,setisloaded]=useState(false);
+  
+  useEffect(
+    ()=>{
+      fetch("http://universities.hipolabs.com/search?country=India").then((data)=>{
+        return data.json();
+
+      }).then((d)=>{
+        setapi(d);
+        console.log(d);
+
+      }).then(()=>{
+        // console.log(api);
+        setisloaded(true);
+      })
+    },[]
+  )
+
+
+  return !isloaded? (
     <div className="App">
-      Hello react
+      App is Loading.................
     </div>
-  );
+  ):(
+    <div>
+      {
+        api.map((i,index)=>{
+          return <div onClick={(e)=>{
+            let d= e.target.value;
+       }} key={index} >{i.name} <button value={index}>See Details</button></div>
+        })
+      }
+      
+      </div>
+  )
 }
 
 export default App;
